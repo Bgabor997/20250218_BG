e0eba4afa4e586ce0aad218f962284708d7866ab
Balics Gábor <balicsgabor@gmail.com>
2025-02-18 15:39:33 +0100

    model,migration,controller

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HarborController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group([
+    "prefix" => "harbors",
+    //use App\Http\Controllers\HarborController
+    "controller" => HarborController::class 
+], function () {
+    Route::get("/", "getAll");
 });
